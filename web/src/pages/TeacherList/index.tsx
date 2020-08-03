b3a3445e1bc394b8eb7ecec5d62cebfe553af532
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-03 20:35:05 -0300

    feat: teacher list header style

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
 
 const TeacherList: React.FC = () => {
-  return <div />;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+
+          <img src={logoImg} alt="Proffy" />
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis.</strong>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default TeacherList;
