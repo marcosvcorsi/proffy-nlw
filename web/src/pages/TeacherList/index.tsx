2deb1c16b8426c00c4c9449b5540eb90ea157562
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-03 21:26:24 -0300

    feat: teacher list mobile first

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 const TeacherList: React.FC = () => {
@@ -27,6 +29,39 @@ const TeacherList: React.FC = () => {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars3.githubusercontent.com/u/5308575?s=460&u=3edc49708de269c0df62f84c1681d6a5582f2ec4&v=4"
+              alt="Marcos"
+            />
+
+            <div>
+              <strong>Marcos Corsi</strong>
+              <span>Programação</span>
+            </div>
+          </header>
+
+          <p>
+            Teste
+            <br /> <br />
+            Teste
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 };
