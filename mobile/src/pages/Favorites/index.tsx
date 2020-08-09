e8360dc45c7a7182d00efff7e07ab6c5434e7676
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-08 21:42:35 -0300

    feat: study tabs finished

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 const Favorites: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  );
 };
 
 export default Favorites;
