be94f9913f85b424cb8e2215a94a1ace85318625
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-10 21:34:35 -0300

    refactor: db connection using env variables

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,12 +1,14 @@
 import path from 'path';
 
+require('dotenv').config();
+
 module.exports = {
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'docker',
-    database: 'proffy_nlw',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
